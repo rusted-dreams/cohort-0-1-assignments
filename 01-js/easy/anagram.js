d192329a000b43f5168ea936bf50c02b650214ae
rusted-dreams <123829783+rusted-dreams@users.noreply.github.com>
2023-12-06 19:29:32 +0530

    solved my first assignment of the course, isAnagram.js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -6,6 +6,21 @@
 
 function isAnagram(str1, str2) {
 
+  // Convert both the strings to lower case.
+  let sortStr1 = str1.toLowerCase();
+  let sortStr2 = str2.toLowerCase();
+
+  // sorting both strings alphabetically:
+  sortStr1 = sortStr1.split('').sort().join('');
+  sortStr2 = sortStr2.split('').sort().join('');
+
+  // if both sorted strings matches then its a anagram.
+  if (sortStr1 === sortStr2) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
+console.log(isAnagram('Hello', 'Olleh'));
 module.exports = isAnagram;
